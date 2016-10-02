af3cae816b1a4697822fd4e74866c0c460b27241
Cooper Maa <coopermaa77@gmail.com>
2016-10-02 09:43:24 +0000

    Seeding data && rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+rng = Random.new
+ +30.times do |i|
+  topic = Topic.create title: "Topic no. #{i+1}", description: "這是用種子建立的第 #{i+1} 個 topic"
+  votes_count = rng.rand(30)
+  votes_count.times { topic.votes.create }
+end
+
+puts "Seeding completed"
